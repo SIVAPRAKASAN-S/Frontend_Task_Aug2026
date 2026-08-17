05d339f3c3d0d7084e385968bd4a6e0b41901292
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-08-17 19:09:04 +0530

    commit-task-1

---
+++ index.js
@@ -0,0 +1,12 @@
+function addnumbers() {
+    var x =  parseInt(document.getElementById("first-number").value) + parseInt(document.getElementById("second-number").value);
+    window.alert(x);
+}
+
+
+let x=4;
+let y=5;
+let z;
+
+ let sum = Math.max(x, y);
+console.log(sum);
