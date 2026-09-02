86c501fe567844f737243cb358d3e5013d490947
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-09-02 19:01:38 +0530

    new commit

--- index.js
+++ index.js
@@ -1,12 +1,76 @@
-function addnumbers() {
-    var x =  parseInt(document.getElementById("first-number").value) + parseInt(document.getElementById("second-number").value);
-    window.alert(x);
-}
+// function addnumbers() {
+//     var x =  parseInt(document.getElementById("first-number").value) + parseInt(document.getElementById("second-number").value);
+//     window.alert(x);
+// }
 
 
-let x=4;
-let y=5;
-let z;
+// let x=4;
+// let y=5;
+// let z;
 
- let sum = Math.max(x, y);
-console.log(sum);
+//  let sum = Math.max(x, y);
+// console.log(sum);
+
+// let number=[10,20,30,40,50];
+
+// function subtractnumber(element){
+// 	return element*2;
+// }
+
+// let result=number.map(subtractnumber);
+
+// console.log(result);
+
+
+
+
+// const names = ['NAVEEN', 'JAYASHRI', 'SHYLA', 'MARIYA', 'SIVA'];
+
+// function subtractnumber(element){
+// 	return element  + "- LENGTH :" + element.length; 
+// }
+
+// let result=names.map(subtractnumber);
+
+// console.log(result);
+
+
+// NAVEEN - LENGTH:67
+
+
+
+// formate data
+
+// const names = ['NAVEEN', 'JAYASHRI', 'SHYLA', 'MARIYA', 'SIVA'];
+// const longwords = names.filter(getlongwords);
+// console.log(longwords);
+
+// function getlongwords(element){
+// 	return element.length >=3;
+// }
+
+
+
+
+// const price=[200,30,900,10,600];
+
+// const total = price.reduce(sum);
+// console.log(total);
+
+// function sum (accumulator, currentvalue){
+// 	return accumulator + currentvalue;
+// }
+
+
+
+
+// const grades = [10, 20, 300, 40, 50];
+
+// const max = grades.reduce(maximum);
+// console.log(max);
+
+// function maximum(accumulator, currentvalue){	
+// 	return Math.max(accumulator, currentvalue);
+// }
+
+
