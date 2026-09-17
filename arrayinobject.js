64cf2cf2419b26755940c904c5ff623d560c5bb3
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-09-17 18:58:41 +0530

    callback commit

--- arrayinobject.js
+++ arrayinobject.js
@@ -1,38 +1,38 @@
-let students = [
-    {
-        name: "Siva",
-        age: 22,
-        department: "CSE"
-    },
-    {
-        name: "Arun",
-        age: 21,
-        department: "ECE"
-    },
-    {
-        name: "Kumar",
-        age: 22,
-        department: "IT"
-    }
-];
+// let students = [
+//     {
+//         name: "Siva",
+//         age: 22,
+//         department: "CSE"
+//     },
+//     {
+//         name: "Arun",
+//         age: 21,
+//         department: "ECE"
+//     },
+//     {
+//         name: "Kumar",
+//         age: 22,
+//         department: "IT"
+//     }
+// ];
 
-console.log(students);
-
-
-for (let i = 0; i < students.length; i++) {
-    console.log(students[i].name);
-    console.log(students[i].age);
-    console.log(students[i].department);
-}
+// console.log(students);
 
 
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i].name);
+//     console.log(students[i].age);
+//     console.log(students[i].department);
+// }
 
-let employees = [
-    { id: 101, name: "Siva", salary: 30000 },
-    { id: 102, name: "Arun", salary: 35000 },
-    { id: 103, name: "Kumar", salary: 40000 }
-];
 
-employees.forEach(function(employee) {
-    console.log(employee.name + " - " + employee.salary);
-});
+
+// let employees = [
+//     { id: 101, name: "Siva", salary: 30000 },
+//     { id: 102, name: "Arun", salary: 35000 },
+//     { id: 103, name: "Kumar", salary: 40000 }
+// ];
+
+// employees.forEach(function(employee) {
+//     console.log(employee.name + " - " + employee.salary);
+// });
--- synchroashychro.js
+++ synchroashychro.js
@@ -1,52 +1,129 @@
-// handlewith : callback ,Promises, async/await
+// // handlewith : callback ,Promises, async/await
 
-function greet(name, callback) {
-    console.log("Hello " + name);
-    callback();
+// function greet(name, callback) {
+//     console.log("Hello " + name);
+//     callback();
+// }
+
+// function message() {
+//     console.log("Welcome to JavaScript");
+// }
+
+// console.log("Start");
+
+// greet("Siva", message);
+
+// console.log("End");
+
+
+
+
+// function calculate(a, b, callback) {
+//     let result = a + b;
+//     callback(result);
+// }
+
+// calculate(10, 20, function(result) {
+//     console.log(result);
+// });
+
+// console.log("Finished");
+
+
+// function greet(callback) {
+//     setTimeout(function () {
+//         console.log("Hello Siva");
+//         callback();
+//     }, 2000);
+// }
+
+// function message() {
+//     console.log("Welcome to JavaScript");
+// }
+
+// console.log("Start");
+
+// greet(message);
+
+// console.log("End");
+
+
+
+function task1(callback){
+    setTimeout(()=>{
+        console.log("Task 1 is completed");
+        callback();
+        },3000);
+}
+function task2(callback){
+    setTimeout(()=>{
+        console.log("Task 2 is completed");
+       callback();
+        },2000);
 }
 
-function message() {
-    console.log("Welcome to JavaScript");
+function task3(callback){
+    setTimeout(()=>{
+        console.log("Task 3 is completed");
+        callback();
+    },3000);
 }
 
-console.log("Start");
-
-greet("Siva", message);
-
-console.log("End");
-
-
-
-
-function calculate(a, b, callback) {
-    let result = a + b;
-    callback(result);
+function task4(callback){
+    setTimeout(()=>{
+        console.log("Task 4 is completed");
+        callback();
+        },1000);
 }
+task1(()=>{
+    task2(()=>{
+        task3(()=>{
+            task4(()=>{
 
-calculate(10, 20, function(result) {
-    console.log(result);
+            });
+        });
+    });
 });
 
-console.log("Finished");
 
 
-function greet(callback) {
-    setTimeout(function () {
-        console.log("Hello Siva");
-        callback();
-    }, 2000);
-}
+// function task1(callback) {
+//     setTimeout(() => {
+//         console.log("Task 1 is completed");
+//         callback();
+//     }, 3000);
+// }
 
-function message() {
-    console.log("Welcome to JavaScript");
-}
+// function task2(callback) {
+//     setTimeout(() => {
+//         console.log("Task 2 is completed");
+//         callback();
+//     }, 2000);
+// }
 
-console.log("Start");
+// function task3(callback) {
+//     setTimeout(() => {
+//         console.log("Task 3 is completed");
+//         callback();
+//     }, 3000);
+// }
 
-greet(message);
+// function task4(callback) {
+//     setTimeout(() => {
+//         console.log("Task 4 is completed");
+//         callback();
+//     }, 1000);
+// }
 
-console.log("End");
-
+// task1(() => {
+//     task2(() => {
+//         task3(() => {
+//             task4(() => {
+//                 console.log("All tasks completed");
+//             });
+//         });
+//     });
+// });
 
 
 
